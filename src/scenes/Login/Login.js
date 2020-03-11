8bae1293db69864d9f436572717002f1ee32613d
michellepennat <alejandrapenna15@gmail.com>
2020-03-10 19:49:53 -0500

    refactor: style modal code activation

--- src/scenes/Login/Login.js
+++ src/scenes/Login/Login.js
@@ -7,6 +7,7 @@ export default function HomeScreen({ navigation }) {
 
   const [modalRegister, setModalRegister] = useState(false)
   const [modalPassword, setModalPassword] = useState(false)
+  const [modalActivate, setModalActivate] = useState(false)
   const [modalCode, setModalCode] = useState(0)
 
   return (
@@ -174,8 +175,67 @@ export default function HomeScreen({ navigation }) {
                   </Item>
                 </Form>
               </LinearGradient>
-              <Button full style={{ backgroundColor: '#417CCA', color: '#162CA3', borderColor: 'transparent', elevation: 0, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }} >
+              <Button onPress={() => alert('holaaaaaaaa')} full style={{ backgroundColor: '#417CCA', color: '#162CA3', borderColor: 'transparent', elevation: 0, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }} >
                 <Text style={{ color: '#FFFFFF' }}> Registrarme </Text>
+              </Button>
+            </View>
+          </LinearGradient>
+        </Container>
+      </Modal>
+
+
+
+      {/* MODAL CODIGO DE ACTIVACION */}
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalActivate}>
+        <Container style={{ height: '100%', backgroundColor: '#000000' }}>
+          <LinearGradient
+            colors={['#072148', '#000000']}
+            style={{ height: '100%', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ height: '80%', width: '90%', backgroundColor: 'transparent' }}>
+              <TouchableOpacity onPress={() => setModalActivate(!modalActivate)} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', marginBottom: 8 }}>
+                <Text style={{ color: '#FFFFFF', textAlign: 'right' }}>Cerrar</Text>
+                <Image
+                  source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583451323/closeIcon.png' }}
+                  style={{ height: 15, width: 15, resizeMode: 'contain', alignItems: 'flex-end', marginLeft: 15 }}
+                />
+              </TouchableOpacity>
+              <LinearGradient
+                colors={['#9DDCFF', '#19428D']}
+                style={{ height: '100%', width: '100%', alignItems: 'center', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
+                <Image style={{ marginTop: 32, width: 300, height: 100, resizeMode: 'contain', backgroundColor: 'transparent' }} source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583315329/Logo.png' }} />
+                <Text style={{ marginTop: 24, marginBottom: 13, color: '#282828', textAlign: 'center' }}>Completa la siguiente información para realizar tu registro.</Text>
+                <Form style={{ paddingRight: 15 }}>
+                  <Item style={{ borderWidth: 0, borderColor: 'transparent', height: 90, width: 320 }} stackedLabel>
+                    <Label style={{ marginBottom: 15, color: '#FFFF' }}>
+                      Número de identificación
+                    </Label>
+                    <Input style={{ borderWidth: 1, borderColor: '#FFFF', borderRadius: 10, backgroundColor: '#FFFFFF1A', width: 320 }} />
+                  </Item>
+                  <Item style={{ borderWidth: 0, borderColor: 'transparent', height: 90 }} stackedLabel last>
+                    <Label style={{ marginBottom: 15, color: '#FFFF' }}>
+                      Nombre y apellido
+                    </Label>
+                    <Input style={{ borderWidth: 1, borderColor: '#FFFF', borderRadius: 10, backgroundColor: '#FFFFFF1A', width: 320 }} />
+                  </Item>
+                  <Item style={{ borderWidth: 0, borderColor: 'transparent', height: 90 }} stackedLabel last>
+                    <Label style={{ marginBottom: 15, color: '#FFFF' }}>
+                      Correo Electrónico
+                    </Label>
+                    <Input style={{ borderWidth: 1, borderColor: '#FFFF', borderRadius: 10, backgroundColor: '#FFFFFF1A', width: 320 }} />
+                  </Item>
+                  <Item style={{ borderWidth: 0, borderColor: 'transparent', height: 90 }} stackedLabel last>
+                    <Label style={{ marginBottom: 15, color: '#FFFF' }}>
+                      Número de celular
+                    </Label>
+                    <Input style={{ borderWidth: 1, borderColor: '#FFFF', borderRadius: 10, backgroundColor: '#FFFFFF1A', width: 320 }} />
+                  </Item>
+                </Form>
+              </LinearGradient>
+              <Button full style={{ backgroundColor: '#417CCA', color: '#162CA3', borderColor: 'transparent', elevation: 0, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }} >
+                <Text style={{ color: '#FFFFFF' }}> Iniciar Sesión </Text>
               </Button>
             </View>
           </LinearGradient>
