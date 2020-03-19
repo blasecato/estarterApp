f9b110b45589a9cdd49789e256d846f78aa4d373
michellepennat <alejandrapenna15@gmail.com>
2020-03-19 04:46:35 -0500

    refactor: modalNuevaRuta

---
+++ src/components/ModalNuevaRuta/ModalNuevaRuta.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Container, Header, Content, Button, Text, Form, Item, Input, Label, View, Alert, Right } from 'native-base';
+import { Image, Modal, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import styles from './ModalNuevaRuta.style';
+
+export default function ModalNuevaRuta({ navigation }) {
+
+    return (
+        <Modal
+            animationType="fade"
+            transparent={false}
+            visible={true}>
+
+            <LinearGradient
+                colors={['#072148D9', '#000000D9']}
+                style={styles.container}>
+                <View style={styles.container}>
+                    <View style={styles.row}>
+                        <View style={[styles.box, styles.boxOne]}>
+                            <Image style={styles.imageClose} source={require('./images/cerrar.png')} />
+                        </View>
+                    </View>
+                    <View style={styles.row}>
+                        <View style={[styles.box, styles.boxTwo]}>
+                            <View style={styles.card}>
+                                <Image style={{ marginBottom: 8 }} source={require('./images/rutas.png')} />
+                                <Text style={{ textAlign: 'center', marginBottom: 30 }}>Ingresa el código para asignar una nueva ruta.</Text>
+                                <LinearGradient
+                                    colors={['#FFA51F', '#EF940C']}
+                                    style={styles.linearStyle}>
+                                </LinearGradient>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </LinearGradient>
+        </Modal>
+    );
+}
