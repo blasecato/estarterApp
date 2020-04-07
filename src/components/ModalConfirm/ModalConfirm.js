7940ad5b1982e31af937871513ac58989f73b9cb
darian7 <yoj.cabrera@udla.edu.co>
2020-04-07 15:09:40 -0500

    feact(ModalActivacion)

--- src/components/ModalConfirm/ModalConfirm.js
+++ src/components/ModalConfirm/ModalConfirm.js
@@ -6,79 +6,79 @@ import styles from './ModalConfirm.style';
 
 export default function ModalConfirm({ navigation, hidden, setHidden, setModalActivation }) {
 
-    return (
-        <Modal
-            animationType="fade"
-            transparent={false}
-            visible={hidden != 0}>
-            <View style={styles.container}>
-                <View style={[styles.box, styles.box1]}>
-                    <LinearGradient
-                        colors={['#072148D9', '#000000D9']}
-                        start={{ x: 0.0, y: 0.5 }}
-                        end={{ x: 0.0, y: 0.9 }}
-                        style={styles.container__background}>
-                        <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
-                            <View style={styles.content}>
-                                <TouchableOpacity
-                                    style={styles.touchClose}
-                                    onPress={() => {
-                                        setHidden(false)
-                                    }}>
-                                    <View style={styles.viewClose}>
-                                        <Text style={styles.textClose}>Cerrar</Text>
-                                        <Image
-                                            source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583451323/closeIcon.png' }}
-                                            style={styles.imageClose}
-                                        />
-                                    </View>
-                                </TouchableOpacity>
-                                <LinearGradient
-                                    colors={['#9DDCFF', '#19428D']}
-                                    style={styles.linearGradient}>
-                                    <Image style={styles.logo}
-                                        source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583315329/Logo.png' }} />
-                                    {
-                                        hidden == 1 ?
-                                            <>
-                                                <View style={styles.cardCircle}>
-                                                    <Image style={styles.imageCardCircle}
-                                                        source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583893414/celular.png' }} />
-                                                </View>
-                                                <Text style={styles.label}>¿Cuál es tu número de celular?</Text>
-                                                <Form style={styles.form}>
-                                                    <Item style={styles.item} stackedLabel last>
-                                                        <Input style={styles.input} />
-                                                    </Item>
-                                                </Form>
-                                                <Button onPress={() => { setModalActivation(1) }} full style={styles.buttonPass} >
-                                                    <Text uppercase={false} style={styles.buttonPass__text}> Restablecer contraseña </Text>
-                                                </Button>
-                                            </>
-                                            :
-                                            <>
-                                                <View style={styles.cardCircle}>
-                                                    <Image style={styles.imageCardCircle}
-                                                        source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583893414/msj.png' }} />
-                                                </View>
-                                                <Text style={styles.label}>¿Cuál es tu correo electrónico?</Text>
-                                                <Form style={styles.form}>
-                                                    <Item style={styles.item} stackedLabel last>
-                                                        <Input style={styles.input} />
-                                                    </Item>
-                                                </Form>
-                                                <Button onPress={() => { setModalActivation(2) }} full style={styles.buttonPass} >
-                                                    <Text uppercase={false} style={styles.buttonPass__text}> Restablecer contraseña </Text>
-                                                </Button>
-                                            </>
-                                    }
-                                </LinearGradient>
-                            </View>
-                        </ScrollView>
-                    </LinearGradient>
-                </View>
-            </View>
-        </Modal>
+	return (
+		<Modal
+			animationType="fade"
+			transparent={false}
+			visible={hidden != 0}>
+			<View style={styles.container}>
+				<View style={[styles.box, styles.box1]}>
+					<LinearGradient
+						colors={['#072148D9', '#000000D9']}
+						start={{ x: 0.0, y: 0.5 }}
+						end={{ x: 0.0, y: 0.9 }}
+						style={styles.container__background}>
+						<ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+							<View style={styles.content}>
+								<TouchableOpacity
+									style={styles.touchClose}
+									onPress={() => {
+										setHidden(false)
+									}}>
+									<View style={styles.viewClose}>
+										<Text style={styles.textClose}>Cerrar</Text>
+										<Image
+											source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583451323/closeIcon.png' }}
+											style={styles.imageClose}
+										/>
+									</View>
+								</TouchableOpacity>
+								<LinearGradient
+									colors={['#9DDCFF', '#19428D']}
+									style={styles.linearGradient}>
+									<Image style={styles.logo}
+										source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583315329/Logo.png' }} />
+									{
+										hidden == 1 ?
+											<>
+												<View style={styles.cardCircle}>
+													<Image style={styles.imageCardCircle}
+														source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583893414/celular.png' }} />
+												</View>
+												<Text style={styles.label}>¿Cuál es tu número de celular?</Text>
+												<Form style={styles.form}>
+													<Item style={styles.item} stackedLabel last>
+														<Input style={styles.input} />
+													</Item>
+												</Form>
+												<Button onPress={() => { setModalActivation(1) }} full style={styles.buttonPass} >
+													<Text uppercase={false} style={styles.buttonPass__text}> Restablecer contraseña </Text>
+												</Button>
+											</>
+											:
+											<>
+												<View style={styles.cardCircle}>
+													<Image style={styles.imageCardCircle}
+														source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583893414/msj.png' }} />
+												</View>
+												<Text style={styles.label}>¿Cuál es tu correo electrónico?</Text>
+												<Form style={styles.form}>
+													<Item style={styles.item} stackedLabel last>
+														<Input style={styles.input} />
+													</Item>
+												</Form>
+												<Button onPress={() => { setModalActivation(2) }} full style={styles.buttonPass} >
+													<Text uppercase={false} style={styles.buttonPass__text}> Restablecer contraseña </Text>
+												</Button>
+											</>
+									}
+								</LinearGradient>
+							</View>
+						</ScrollView>
+					</LinearGradient>
+				</View>
+			</View>
+		</Modal>
 
-    );
+	);
 }
--- src/components/ModalSignup/ModalSignup.js
+++ src/components/ModalSignup/ModalSignup.js
@@ -5,76 +5,81 @@ import { LinearGradient } from 'expo-linear-gradient';
 import styles from './ModalSignup.style';
 import { ScrollView } from 'react-native-gesture-handler';
 
-export default function ModalSignup({ navigation, hidden, setHidden, setModalCode, setModalActivation }) {
+export default function ModalSignup({ navigation, hidden, setHidden, setModalCode, setModalActivation, setPhone, setEmail }) {
 
-    return (
-        <Modal
-            animationType="fade"
-            transparent={false}
-            visible={hidden}>
-            <View style={styles.container}>
-                <View style={[styles.box, styles.box1]}>
-                    <LinearGradient
-                        colors={['#072148D9', '#000000D9']}
-                        start={{ x: 0.0, y: 0.5 }}
-                        end={{ x: 0.0, y: 0.9 }}
-                        style={styles.container__background}>
-                        <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
-                            <View style={styles.content}>
-                                <TouchableOpacity
-                                    style={styles.touchClose}
-                                    onPress={() => {
-                                        setHidden(false)
-                                    }}>
-                                    <View style={styles.viewClose}>
-                                        <Text style={styles.textClose}>Cerrar</Text>
-                                        <Image
-                                            source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583451323/closeIcon.png' }}
-                                            style={styles.imageClose}
-                                        />
-                                    </View>
-                                </TouchableOpacity>
-                                <LinearGradient
-                                    colors={['#9DDCFF', '#19428D']}
-                                    style={styles.linearGradient}>
-                                    <Image style={styles.logo}
-                                        source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583315329/Logo.png' }} />
-                                    <Text style={styles.textRegister}>Completa la siguiente información para realizar tu registro.</Text>
-                                    <Form style={styles.form}>
-                                        <Item style={styles.item} stackedLabel>
-                                            <Label style={styles.label}>
-                                                Número de identificación
+
+	return (
+		<Modal
+			animationType="fade"
+			transparent={false}
+			visible={hidden}>
+			<View style={styles.container}>
+				<View style={[styles.box, styles.box1]}>
+					<LinearGradient
+						colors={['#072148D9', '#000000D9']}
+						start={{ x: 0.0, y: 0.5 }}
+						end={{ x: 0.0, y: 0.9 }}
+						style={styles.container__background}>
+						<ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+							<View style={styles.content}>
+								<TouchableOpacity
+									style={styles.touchClose}
+									onPress={() => {
+										setHidden(false)
+									}}>
+									<View style={styles.viewClose}>
+										<Text style={styles.textClose}>Cerrar</Text>
+										<Image
+											source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583451323/closeIcon.png' }}
+											style={styles.imageClose}
+										/>
+									</View>
+								</TouchableOpacity>
+								<LinearGradient
+									colors={['#9DDCFF', '#19428D']}
+									style={styles.linearGradient}>
+									<Image style={styles.logo}
+										source={{ uri: 'https://res.cloudinary.com/cacaotics/image/upload/v1583315329/Logo.png' }} />
+									<Text style={styles.textRegister}>Completa la siguiente información para realizar tu registro.</Text>
+									<Form style={styles.form}>
+										<Item style={styles.item} stackedLabel>
+											<Label style={styles.label}>
+												Número de identificación
                                     </Label>
-                                            <Input style={styles.input} />
-                                        </Item>
-                                        <Item style={styles.item} stackedLabel>
-                                            <Label style={styles.label}>
-                                                Nombre y apellido
+											<Input style={styles.input} />
+										</Item>
+										<Item style={styles.item} stackedLabel>
+											<Label style={styles.label}>
+												Nombre y apellido
                                     </Label>
-                                            <Input style={styles.input} />
-                                        </Item>
-                                        <Item style={styles.item} stackedLabel>
-                                            <Label style={styles.label}>
-                                                Correo Electrónico
+											<Input style={styles.input} />
+										</Item>
+										<Item style={styles.item} stackedLabel>
+											<Label style={styles.label}>
+												Correo Electrónico
                                     </Label>
-                                            <Input style={styles.input} />
-                                        </Item>
-                                        <Item style={styles.item} stackedLabel last>
-                                            <Label style={styles.label}>
-                                                Número de celular
+											<Input style={styles.input}
+												onChangeText={(text) => setEmail(text)}
+											/>
+										</Item>
+										<Item style={styles.item} stackedLabel last>
+											<Label style={styles.label}>
+												Número de celular
                                     </Label>
-                                            <Input style={styles.input} />
-                                        </Item>
-                                    </Form>
-                                </LinearGradient>
-                                <Button onPress={() => setModalActivation(3)} full style={styles.button} >
-                                    <Text uppercase={false} style={styles.textButton}> Registrarme </Text>
-                                </Button>
-                            </View>
-                        </ScrollView>
-                    </LinearGradient>
-                </View>
-            </View>
-        </Modal>
-    );
+											<Input style={styles.input}
+												onChangeText={(text) => setPhone(text)}
+											/>
+										</Item>
+									</Form>
+								</LinearGradient>
+								<Button onPress={() => setModalActivation(3)} full style={styles.button} >
+									<Text uppercase={false} style={styles.textButton}> Registrarme </Text>
+								</Button>
+							</View>
+						</ScrollView>
+					</LinearGradient>
+				</View>
+			</View>
+		</Modal>
+	)
 }
